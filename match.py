bef1ea83f23d25b99d0c3a8895755ecb3e29ab83
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2023-01-04 20:09:26 -0300

    Click module basic options to improve CLI

--- match.py
+++ match.py
@@ -1,26 +1,63 @@
 import sys
 import os
+import click
 
 from difflib import SequenceMatcher
 
 TARGET_FILES_EXT = '.mem'
 
-filesList1 = []
-filesList2 = []
+@click.command()
+@click.option('-s', '--single_path', default='null', help='Similarity evaluation within a single path.')
+@click.option('-m', '--multi_path', type=(str,str), default=('null','null'), help='Similarity evaluation considering a combination of two paths.')
 
-def fillFilesList(argPath):
-    returnable = []
+def checkSimilarity(single_path, multi_path):
+    filesList1 = []
+    filesList2 = []
+    
+    handleFilesList(single_path, multi_path, filesList1, filesList2)
+    
+    reportList = compareFilesLists(filesList1, filesList2)
+    exportReportAsTsv(reportList)
+
+
+def handleFilesList(single_path, multi_path, filesList1, filesList2):
+    print(single_path, multi_path)
+    if single_path != 'null':
+        fillFilesList(filesList1, single_path)
+        fillFilesList(filesList2, single_path)
+    elif multi_path[0] != 'null':
+        fillFilesList(filesList1,multi_path[0])
+        fillFilesList(filesList2,multi_path[1])
+    else:
+        print(f'[Error] A single or multi path option must be informed!')
+        exit()
+    
+
+def fillFilesList(filesList, argPath):
     if os.path.isfile(argPath):
-        returnable.append(os.path.abspath(argPath)) #FIX: insert the complete path
+        filesList.append(os.path.abspath(argPath)) #FIX: insert the complete path
     elif os.path.isdir(argPath):
         for path, subdirs, files in os.walk(argPath):
             for name in files:
-                returnable.append(os.path.abspath(f'{path}/{name}'))
+                filesList.append(os.path.abspath(f'{path}/{name}'))
     else:
         print(f'[Error] Given path ({argPath}) is not a file neither a folder')
         exit()
     
-    return returnable
+def compareFilesLists(filesList1, filesList2):
+    reportList = []
+    totalComps = len(filesList1) * len(filesList2)
+
+    idComp = 0
+    for file1 in filesList1:
+        for file2 in filesList2:       
+            if file1 != file2 and file1.endswith(TARGET_FILES_EXT) and file2.endswith(TARGET_FILES_EXT):
+                status = float(idComp)/totalComps * 100
+                print(f'[Status] {status:.1f}%')
+                reportList.append(matchFiles(file1, file2))
+            idComp += 1
+    
+    return reportList
 
 def matchFiles(file1, file2):
     text1 = open(file1).read()
@@ -33,38 +70,18 @@ def matchFiles(file1, file2):
         'file2' : file2,
         'similarity' : similarity * 100
         }
-   
-def generateTsvLine(r):
-    return f'{r["similarity"]}\t{r["file1"]}\t{r["file2"]}\n'
 
-if __name__ == '__main__':
-
-    if len(sys.argv) != 3:
-        print(f'[Error] Wrong number of arguments (expected 2, given {len(sys.argv)-1})')
-        exit()
-
-    filesList1 = fillFilesList(sys.argv[1])
-    filesList2 = fillFilesList(sys.argv[2])
-
-    print(f'[DBG] Files list 1: {filesList1}')
-    print(f'[DBG] Files list 2: {filesList2}')
-
-    reportList = []
-
-    totalComps = len(filesList1) * len(filesList2)
-
-    idComp = 0
-    for file1 in filesList1:
-        for file2 in filesList2:       
-            if file1 != file2 and file1.endswith(TARGET_FILES_EXT) and file2.endswith(TARGET_FILES_EXT):
-                print(f'[DBG] Comp {idComp} of {totalComps}')
-                print(file1)
-                print(file2 + '\n\n')
-                reportList.append(matchFiles(file1, file2))
-            idComp += 1
-    
+def exportReportAsTsv(reportList):
     fpReport = open("report.tsv", "w")
     fpReport.write('similarity\tfile_1\tfile_2\n')
     for report in reportList:
         fpReport.write(generateTsvLine(report))
     fpReport.close()
+
+def generateTsvLine(r):
+    return f'{r["similarity"]}\t{r["file1"]}\t{r["file2"]}\n'
+
+
+
+if __name__ == '__main__':
+    checkSimilarity()
